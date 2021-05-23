6643842f4603e1e4c886c6d3329a3aa555a1f2b0
Titus Ng <sadlyotisfying@gmail.com>
2021-05-23 16:16:43 +0800

    2nd try

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require("http");
 var PORT = process.env.PORT||3000;
 
 const server = http.createServer(function(req, res) {
-    res.end(req.socket.remoteAddress);
+    res.end(req.headers["x-forwarded-for"]);
 });
 
 server.listen(PORT);
