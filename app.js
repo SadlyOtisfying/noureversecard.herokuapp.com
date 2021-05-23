eab7db9e16c28d6b06803765f1077dc0b75c07d6
Titus Ng <titusngtszhin@gmail.com>
2021-05-23 17:20:52 +0800

    logs ip

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const fs = require("fs");
 var PORT = process.env.PORT||3000;
 
 const server = http.createServer(function(req, res) {
+    var ip = req.headers["x-forwarded-for"]||req.socket.remoteAddress.replace("::ffff:", "");
     if(req.method==="GET"&&req.url==="/favicon.ico") {
         res.setHeader('Content-Type', 'image/x-icon');
         fs.createReadStream("favicon.ico").pipe(res);
     }
     else
-        res.end(req.headers["x-forwarded-for"]);
+        res.end(ip);
+    console.log(ip)
 });
 
 server.listen(PORT);
