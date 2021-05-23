2393fb9b145219d37030dd1d93a205ba114711b2
Titus Ng <titusngtszhin@gmail.com>
2021-05-23 17:11:27 +0800

    added favicon

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const http = require("http");
+const fs = require("fs");
 
 var PORT = process.env.PORT||3000;
 
 const server = http.createServer(function(req, res) {
-    res.end(req.headers["x-forwarded-for"]);
+    if(req.method==="GET"&&req.url==="/favicon.ico") {
+        res.setHeader('Content-Type', 'image/x-icon');
+        fs.createReadStream("favicon.ico").pipe(res);
+    }
+    else
+        res.end(req.headers["x-forwarded-for"]);
 });
 
 server.listen(PORT);
