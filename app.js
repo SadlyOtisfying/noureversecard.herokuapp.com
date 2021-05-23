3db8c074c1ccb05cccd0fdbee2d814adb37e5d93
Titus Ng <titusngtszhin@gmail.com>
2021-05-23 17:54:06 +0800

    typo

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ var PORT = process.env.PORT||3000;
 
 const server = http.createServer(function(req, res) {
     var ip = req.headers["x-forwarded-for"]||req.socket.remoteAddress.replace("::ffff:", "");
-    console.log(ip)
+    console.log(ip + " at " + Date.now().toString());
     if(req.method==="GET"&&req.url==="/favicon.ico") {
         res.setHeader('Content-Type', 'image/x-icon');
         fs.createReadStream("favicon.ico").pipe(res);
     }
     else
-        res.end("Your IP address " + ip + "is logged. ;)");
+        res.end("Your IP address " + ip + " is logged. ;)");
 });
 
 server.listen(PORT);
